1385741c27ab88584c1405edc5a6e8070d526b81
Zak Robertson <zakrobertson1@berkeley.edu>
2025-02-10 12:38:46 +0100

    added table classifying song data by year for use in PowerBI

--- projects/spotify_database/SQL Queries/fill_streams.sql
+++ projects/spotify_database/SQL Queries/fill_streams.sql
@@ -3,3 +3,8 @@ INSERT INTO dbo.streams (trackName, albumName, artistName, trackURI, msPlayed, e
 SELECT trackName, albumName, artistName, trackURI, msPlayed, endTime, IIF (skipped LIKE 'TRUE', 1, 0)
 FROM dbo.raw_data;
 
+DELETE FROM streams
+SELECT * 
+WHERE trackURI IS NULL
+
+
