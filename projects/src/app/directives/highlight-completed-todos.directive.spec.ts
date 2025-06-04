6353e477ac2d3181e1635ef877499930b16d8c96
ZakRobertson <132111368+ZakRobertson@users.noreply.github.com>
2025-06-04 15:07:52 -0700

    Delete projects/src directory

--- projects/src/app/directives/highlight-completed-todos.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightCompletedTodosDirective } from './highlight-completed-todos.directive';
-
-describe('HighlightCompletedTodosDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightCompletedTodosDirective();
-    expect(directive).toBeTruthy();
-  });
-});
